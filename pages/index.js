458b9de8eef93f07254b1e2015232c1e5cb1cf23
vvishal11 <11kumar.vishal4@gmail.com>
2020-08-27 21:08:26 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
 {/*card01 starts*/}
 <div class = "pb-16 max-w-4xl" >
   <div class="bg-white  rounded overflow-hidden shadow-lg">
-    <img class="w-full" src="/card01.jpg" alt="Sunset in the mountains"></img>
+    <img class="w-full" src="/card01.png" alt="Sunset in the mountains"></img>
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">The Coldest Sunrise</div>
       <p class="text-gray-700 text-base">
@@ -126,6 +126,37 @@ export default function Home() {
   </div>
 </div>
 {/*card03 ends*/}
+
+{/*card04 starts*/}
+<div class = "pb-16 max-w-4xl" >
+  <div class="bg-white  rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="/card04.jpg" alt="Sunset in the mountains"></img>
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">The Hottest Midnight</div>
+      <p class="text-gray-700 text-base">
+      Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis
+      ornare vel eu leo. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+      Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis. […]
+      </p>
+    </div>
+    <div class="px-6 py-4">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    </div>
+
+    <div class=" border-t border-gray-300  mt-4 ml-10 mr-10 "></div>
+    <div class="flex">
+    <div class="p-1 px-2 ml-6 mt-6 mb-10 bg-gray-400 inline-block shadow-lg italic"> Category: </div>
+    <div class="p-1 ml-6 mt-6 mb-10 bg-gray-200 inline-block shadow-lg italic"> Uncategorised </div>
+    </div>
+  </div>
+</div>
+{/*card04 ends*/}
+
+
+
+
 
 </div>
 {/*card ends*/}
